2f16e2db23165fe2ac0a879f06d66a98aa9d77cf
xieh1987 <h.xie1987@gmail.com>
2014-07-16 11:29:01 -0400

    add file

---
+++ Valid Parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @return a boolean
+    def isValid(self, s):
+        aList=[]
+        valid=True
+        map={'(':')','[':']','{':'}'}
+        for i in range(len(s)):
+            if s[i] in map:
+                aList.append(s[i])
+            else:
+                if aList!=[]:
+                    valid=(map[aList.pop()]==s[i])
+                    if not valid:
+                        break
+                else:
+                    valid=False
+        return (valid and aList==[])
