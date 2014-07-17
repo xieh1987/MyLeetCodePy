3a3b3a687d07b3a734ef1bbd5563a6792f8768c3
xieh1987 <h.xie1987@gmail.com>
2014-07-17 08:17:39 -0400

    add file

---
+++ Path Sum.py
@@ -0,0 +1,26 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @param sum, an integer
+    # @return a boolean
+    def hasPathSum(self, root, sum):
+        if root==None:
+            return False
+        elif root.left==None and root.right==None:
+            return root.val==sum
+        else:
+            found=False
+            if root.left!=None:
+                found=self.hasPathSum(root.left,sum-root.val)
+            if found:
+                return True
+            else:
+                if root.right!=None:
+                    found=self.hasPathSum(root.right,sum-root.val)
+                return found
---
+++ Triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param triangle, a list of lists of integers
+    # @return an integer
+    def minimumTotal(self, triangle):
+        if len(triangle)==1:
+            return triangle[0][0]
+        else:
+            for row in range(len(triangle)-1,0,-1):
+                for column in range(row):
+                    triangle[row-1][column]+=min(triangle[row][column],triangle[row][column+1])
+            return triangle[0][0]
