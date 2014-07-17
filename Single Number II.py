65d486675a767be9c40c38df194d09be849371d3
xieh1987 <h.xie1987@gmail.com>
2014-07-17 08:27:18 -0400

    add file

---
+++ Single Number II.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        sum=0
+        map={}
+        for i in range(len(A)):
+            if A[i] not in map:
+                map[A[i]]=A[i]
+                sum+=A[i]
+            elif A[i]==map[A[i]]:
+                map[A[i]]+=1
+                sum+=A[i]
+            else:
+                sum-=2*A[i]
+        return sum
---
+++ Single Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        map={}
+        sum=0
+        for i in range(len(A)):
+            if A[i] not in map:
+                sum+=A[i]
+                map[A[i]]=i
+            else:
+                sum-=A[i]
+        return sum
