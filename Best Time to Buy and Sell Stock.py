d7571ef7369f9a956b69d5b80adf5541a5e05b53
xieh1987 <h.xie1987@gmail.com>
2014-07-17 08:23:55 -0400

    add file

---
+++ Best Time to Buy and Sell Stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param prices, a list of integer
+    # @return an integer
+    def maxProfit(self, prices):
+        if len(prices)==0:
+            return 0
+        minPrice=prices[0]
+        maxProfit=0
+        for price in range(len(prices)):
+            minPrice=min(minPrice, prices[price])
+            maxProfit=max(maxProfit, prices[price]-minPrice)
+        return maxProfit
---
+++ Valid Palindrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    # @param s, a string
+    # @return a boolean
+    def isPalindrome(self, s):
+        if len(s)<=1:
+            return True
+        s=s.lower()
+        char='0123456789abcdefghijklmnopqrstuvwxyz'
+        isPali=True
+        aList=list(s)
+        head=0
+        tail=len(aList)-1
+        while head<=tail:
+            while aList[head] not in char and head<tail:
+                head+=1
+            while aList[tail] not in char and tail>head:
+                tail-=1
+            if aList[head]!=aList[tail]:
+                isPali=False
+                break
+            else:
+                head+=1
+                tail-=1
+        return isPali
